1772375ccf4c9c4ad4e75dc6b79f127185f6892a
francus11 <francus62@gmail.com>
2024-05-10 21:03:00 +0200

    Initialize

---
+++ src/test/java/com/bidder/bidder/BidderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidder.bidder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
